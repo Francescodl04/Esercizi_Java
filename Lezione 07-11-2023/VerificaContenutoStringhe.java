0f283b07dc2a15cd2793b3719cd26553b48794cb
Francesco Di Lena <francescodl727@gmail.com>
2023-11-10 11:54:26 +0100

    Altri progressi

--- Lezione 07-11-2023/VerificaContenutoStringhe.java
+++ Lezione 07-11-2023/VerificaContenutoStringhe.java
@@ -10,12 +10,40 @@ public class VerificaContenutoStringhe{
     public static void main(String[] args){
         Scanner console = new Scanner(System.in);
         System.out.println("Benvenuto nel programma. Inserisci la prima stringa:");
-        String s1 = console.nextLine();
-        String s2 = "";
-        do{
-            System.out.println("Inserisci ora la seconda stringa:");
-            String s2 = console.nextLine();
-        } while (s2 == "");
-        for(int i = 0; i < s1.length())
+        String string1 = console.nextLine();
+        System.out.println("Inserisci ora la seconda stringa:");
+        String string2 = console.nextLine();
+        while(string2 == ""){
+            System.out.println("Non hai inserito una stringa valida. Inserisci di nuovo la seconda stringa:");
+            string2 = console.nextLine();
+        } 
+        int startIndex = 0, endIndex = 0;
+        boolean substringCheck = false;
+        for(int i = 0; i < string1.length(); i++){
+            if(i > string2.length()){
+                break;
+            }
+            for(int j = 0; j < string2.length(); j++){
+                if(string2.charAt(j) == string1.charAt(i)){
+                    if(substringCheck == false){
+                        startIndex = i;
+                        endIndex = i;
+                        substringCheck = true;
+                    }
+                    else{
+                        endIndex++;
+                    }
+                }
+                else{
+                    substringCheck == false;
+                    break;
+                }
+            }
+        }
+        if(substringCheck == false){
+            System.out.println("La seconda stringa NON e' contenuta nella prima.");
+            System.exit(1);
+        }
+        System.out.printf("La seconda stringa e' contenuta nella prima dalla posizione %d alla %d.", startIndex, endIndex);        
     }
 }
