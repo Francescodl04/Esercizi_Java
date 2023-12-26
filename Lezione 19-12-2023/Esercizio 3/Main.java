32846c00c83ed169640a8e1321503f9322a7a74b
Francescodl04 <72445602+Francescodl04@users.noreply.github.com>
2023-12-26 16:16:12 +0100

    Proseguito es. 3 lab. 19-12-2023
    Manca una corretta gestione del resize e necessitano di essere controllati tutti gli altri metodi

--- Lezione 19-12-2023/Esercizio 3/Main.java
+++ Lezione 19-12-2023/Esercizio 3/Main.java
@@ -5,10 +5,49 @@
  * Classe main
 */
 
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        MiaCD uno, due, tre;
+        Scanner console = new Scanner(System.in);
+        MiaCD uno = new MiaCD(), due = new MiaCD(), tre = new MiaCD();
+        System.out.println("Benvenuto. Inserisci di seguito, uno per riga, un numero intero (termina inserendo CTRL + Z):");
+        do
+        {
+            try
+            {
+                uno.addLast(Integer.parseInt(console.nextLine()));
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Non hai inserito un numero intero, riprova...");
+                continue;
+            }
+            catch(NoSuchElementException e)
+            {
+                System.exit(0);
+            }
+        }
+        while(console.hasNextLine());
+        
+        while(!uno.isEmpty())
+        {
+            due.addFirst(uno.removeLast());
+        }
+
+        while(!due.isEmpty())
+        {
+            tre.addLast(due.removeFirst());
+        }
+
+        System.out.println("Bene, hai terminato l'inserimento. Ora ti mostro il contenuto di cio' che hai inserito nella coda:");
+
+        while(!tre.isEmpty())
+        {
+            System.out.println(tre.removeFirst());
+        }
     }
 }
--- Lezione 19-12-2023/Esercizio 3/MiaCD.java
+++ Lezione 19-12-2023/Esercizio 3/MiaCD.java
@@ -19,60 +19,122 @@ interface CD extends Container
     void addLast(Object x);
     Object getFirst() throws EmptyCDException;
     Object getLast() throws EmptyCDException;
-    Object removeFirst(Object x) throws EmptyCDException;
-    Object removeLast(Object x) throws EmptyCDException;
+    Object removeFirst() throws EmptyCDException;
+    Object removeLast() throws EmptyCDException;
 }
 
 class MiaCD implements CD
 {
+    private Object[] v;
+    private int size, first, last;
+
+    private final int ARRAY_DIM = 100;
 
     public MiaCD()
     {
-
+        v = new Object[ARRAY_DIM];
+        makeEmpty();
     }
 
     public boolean isEmpty()
     {
-        return false;
+        return first == last;
     }
 
     public void makeEmpty()
     {
-
+        size = 0;
+        first = 0;
+        last = 0;
     }
 
     public int size()
     {
-        return 0;
+        return size;
+    }
+
+    private int increment(int index)
+    {
+        return (index + 1) % v.length;
+    }
+
+    private int decrement(int index)
+    {
+        return (index - 1 + v.length) % v.length;
     }
 
     public void addFirst(Object x)
     {
-
+        if(size == v.length)
+        {
+            int oldLength = v.length;
+            v = resize(v, v.length * 2);
+            if(increment(first) > decrement(last))
+            {
+                Object[] firstPart = new Object[oldLength - first];
+                Object[] secondPart = new Object[last];
+                System.arraycopy(v, 0, secondPart, 0, last);
+                System.arraycopy(v, first, firstPart, 0, oldLength);
+                System.arraycopy(firstPart, 0, v, 0, firstPart.length);
+                System.arraycopy(secondPart, 0, v, firstPart.length - 1, secondPart.length);
+            }
+            first = v.length - 1;
+            last = oldLength;
+        }
+        v[first] = x;
+        first = decrement(first);
+        size++;
     }
 
     public void addLast(Object x)
     {
-
+        if(size == v.length)
+        {
+            v = resize(v, v.length * 2);
+        }
+        v[last] = x;
+        last = increment(last);
+        size++;
     }
 
     public Object getFirst() throws EmptyCDException
     {
-        return null;
+        if(isEmpty()) throw new EmptyCDException();
+
+        return v[increment(first)];
     }
 
     public Object getLast() throws EmptyCDException
     {
-        return null;
+        if(isEmpty()) throw new EmptyCDException();
+
+        return v[decrement(last)];
     }
 
-    public Object removeFirst(Object x) throws EmptyCDException
+    public Object removeFirst() throws EmptyCDException
     {
-        return null;
+        Object temp = getFirst();
+        first = increment(first);
+        v[first] = null;
+        size--;
+        return temp;
     }
 
-    public Object removeLast(Object x) throws EmptyCDException
+    public Object removeLast() throws EmptyCDException
     {
-        return null;
+        Object temp = getLast();
+        last = decrement(last);
+        v[last] = null;
+        size--;
+        return temp;
+    }
+
+    private static Object[] resize(Object[] oldArray, int newLength) throws IllegalArgumentException
+    {
+        if(newLength <= oldArray.length) throw new IllegalArgumentException();
+
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(oldArray, 0, newArray, 0, oldArray.length);
+        return newArray;
     }
 }
