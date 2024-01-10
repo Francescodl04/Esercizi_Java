b26fc9de38933900987f1cb66ddfefb8c8751873
Francesco Di Lena <francescodl727@gmail.com>
2024-01-10 15:44:11 +0100

    Inizio es. in aula 10-01-2024

---
+++ Esercitazioni in aula/10 - 10-01-2024/Traduci.java
@@ -0,0 +1,13 @@
+/*
+ * Francesco Di Lena
+ * Esercizio in aula - Fondamenti di informatica
+ * 10-01-2023
+*/
+
+public class Traduci
+{
+    public static void main (String[] args)
+    {
+        
+    }
+}
